a234098c9f04775ee700fce8e9211be52227bb12
nachocheez <cheskapg@gmail.com>
2025-04-06 23:41:53 +0800

    feat: add @fastify/cors dependency and configure CORS in server setup

--- server.js
+++ server.js
@@ -7,9 +7,19 @@ import fastifySwaggerUI from "@fastify/swagger-ui";
 import customerRoutes from "./routes/customers-routes.js";
 import salesRoutes from "./routes/sales-routes.js";
 import productRoutes from "./routes/products-routes.js";
+import cors from "@fastify/cors";
 const PORT = process.env.PORT || 3000;
 const fastify = Fastify();
-
+await fastify.register(cors, {
+    origin: "*",
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: [
+      'content-type',
+      'accept',
+      'content-type',
+      'authorization'
+    ],
+  })
 const swaggerOptions = {
   openapi: {
     openapi: "3.0.0",
@@ -39,6 +49,8 @@ await fastify.register(fastifySwaggerUI, swaggerUiOptions);
 await fastify.register(itemRoutes); //
 await fastify.register(customerRoutes); //
 await fastify.register(salesRoutes); //
+
+
 fastify.register(productRoutes); //
 
 // fastify.get("/", async (req, reply) => {
