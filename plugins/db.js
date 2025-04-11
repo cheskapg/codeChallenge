aee69523771c07499171238d54dc44005c2245c9
nachocheez <cheskapg@gmail.com>
2025-04-11 16:19:47 +0800

    chore: update database configuration in .env and db.js for remote connection

--- plugins/db.js
+++ plugins/db.js
@@ -8,6 +8,21 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // Accept self-signed certs
+  },
 });
 
 export default pool;
+
+// import 'dotenv/config';
+// import pkg from 'pg';
+// const { Pool } = pkg;
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   // Optional SSL if you're connecting to a remote DB like Heroku or Supabase
+//   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+// });
+
+// export default pool;
